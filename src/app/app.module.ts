17e1ca26bd0d93e49de06d6e4861af9824315d1e
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-03-10 11:26:50 -0300

    adicionando pagina principal e estilo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+
+import { ListWeatherComponent } from "./weather/list-weather/list-weather.component";
+import { SearchWeatherComponent } from "./weather/search-weather/search-weather.component";
+import { HeaderComponent } from "./weather/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListWeatherComponent,
+    SearchWeatherComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
